a2d0b5125041d05a9ba7bd8776c6ba0abcc45684
julmaldo <julio.a.maldo@gmail.com>
2015-09-27 22:09:02 -0500

    upload finished product

--- MaldonadoJulio-hw2/ViewController.swift
+++ MaldonadoJulio-hw2/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        name.delegate = self
+        city.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //makes the keyboard disapear when clicking on something outside the keyboard
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+    
+    //viewcontroller variables
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var savebutton: UIButton!
+    
+    //button handler checks if fields are filled and then changes messageLabel to "name - city"
+    @IBAction func btnSaveClicked(sender: AnyObject) {
+        self.view.endEditing(true)
+        if textFeildFilled(name.text, city: city.text) {
+            messageLabel.text = "\(name.text) - \(city.text)"
+        } else{
+            messageLabel.text = "You must enter a value for *both* name and city!!"
+        }
+    }
+    
+    //dismisses keyboard when return is pressed
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    //check if string contains a value and returns true if there is and false if not
+    func textFeildFilled(name:String,city:String) -> Bool {
+        var result = true
+        if name.isEmpty || city.isEmpty{
+            result = false
+        }
+        return result
+    }
 }
 
